8d89fed20ad27a70bd40c59c8861f57886b4dea9
Igor <alexandre.igor64@gmail.com>
2021-04-12 20:42:06 -0300

    Aula 06

--- aula52/src/app/app.component.ts
+++ aula52/src/app/app.component.ts
@@ -6,12 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aula52';
-  nome = "José";
-  idade = 20
 
+  /*
+
+  title = 'aula52';
+
+  idade = 20
   lancarDado() {
     return Math.floor(Math.random() * 6) + 1;
   }
+
+  
+  alterarNome(nome) {
+    console.log(nome);
+    this.nome = nome;
+  
+  nome;
+  alterarNome(nomeInput) {
+    console.log(nomeInput);
+    console.log("Adicionando...");
+  }
+
+  adicionar() {
+    console.log("Adicionando...");
+  }
+
+  
+}
+nome;
+  esconderCaixa = true;
+  adicionar(nomeInput) {
+    console.log(nomeInput.value);
+    this.nome = nomeInput.value;
+    this.esconderCaixa = nomeInput.value.length <= 0;
+  }
+
+
+*/
+
+  numero: number;
+  escolher() {
+    this.numero = Math.floor(Math.random() * 100) + 1;
+  }
+
+
 
 }
--- aula52/src/app/app.module.ts
+++ aula52/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
